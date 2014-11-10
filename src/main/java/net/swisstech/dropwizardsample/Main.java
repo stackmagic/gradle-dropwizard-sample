f1d7be26c7639943cc859d823a990d15ceef4e08
Patrick Huber <stackmagic@gmail.com>
2014-11-10 14:47:39 +0100

    build/code etc... uses DW only, inttests to follow

--- src/main/java/net/swisstech/dropwizardsample/Main.java
+++ src/main/java/net/swisstech/dropwizardsample/Main.java
@@ -1,8 +1,37 @@
 package net.swisstech.dropwizardsample;
 
-public class Main {
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import net.swisstech.dropwizard.util.DummyHealthCheckBundle;
+import net.swisstech.dropwizard.util.DummyResourceBundle;
+import net.swisstech.dropwizard.util.NullConfig;
+import net.swisstech.dropwizard.util.VersionBundle;
 
-	public static void main(String[] args) {
-		System.out.println("YYYYYYYYYYYYYyy");
+public class Main extends Application<NullConfig> {
+
+	public static void main(String[] args) throws Exception {
+		new Main().run(args);
+	}
+
+	@Override
+	public String getName() {
+		return "dopewizard-sample";
+	}
+
+	@Override
+	public void initialize(Bootstrap<NullConfig> bootstrap) {
+		// DO NOT TOUCH: version banner required for int/acc tests
+		bootstrap.addBundle(new VersionBundle());
+
+		// dummies when an app has neither of these (yet!)
+		// remove these when adding the real thing!
+		bootstrap.addBundle(new DummyResourceBundle());
+		bootstrap.addBundle(new DummyHealthCheckBundle());
+	}
+
+	@Override
+	public void run(NullConfig configuration, Environment environment) throws InterruptedException {
+		// add your resources here
 	}
 }
